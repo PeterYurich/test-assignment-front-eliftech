a3aa10719f44588c9a37cca2efb12221ad288bfa
Peter <peteryurich@gmail.com>
2023-06-02 15:00:54 +0300

    initial

--- src/utils/getOwnerAndRepoFromUrl.js
+++
@@ -1,5 +0,0 @@
-export default function getOwnerAndRepoFromUrl(repoUrl) {
-    const arr = repoUrl.split("/")
-    const [repoOwner, repoName] = [arr[3]?.toLocaleLowerCase(), arr[4]?.toLocaleLowerCase()]
-    return { repoOwner, repoName }
-}
